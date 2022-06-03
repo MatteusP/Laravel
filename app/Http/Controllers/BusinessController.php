19bb7fb073f39a0db69502c328b5676823823584
Mateus Paiva <matteusp@alu.ufc.br>
2022-06-03 10:00:11 -0300

    criando models, controlers e factories

---
+++ app/Http/Controllers/BusinessController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BusinessController extends Controller
+{
+    //Business::all()
+    //./vendor/bin/sail artisan make:model Business --migration --controller --factory
+}
